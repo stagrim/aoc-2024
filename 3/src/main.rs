7ecf8e35513a8963306af76970424f9e3f3bbd56
Esbjörn Stenberg <me@stagrim.com>
2024-12-03 11:08:16 +0100

    day 3 part 2 done

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -2,18 +2,37 @@ use std::io::{stdin, Read};
 
 use regex::Regex;
 
+#[derive(Debug)]
+enum Matched {
+    Mul(isize),
+    Do,
+    DoNot,
+}
+
 fn main() {
     let mut input = String::new();
     stdin()
         .read_to_string(&mut input)
         .expect("Failed to read stdin");
 
-    let sum: isize = Regex::new(r"mul\([0-9]*,[0-9]*\)")
+    let mut enabled = true;
+    let (sum1, sum2): (isize, isize) = Regex::new(r"(mul\([0-9]*,[0-9]*\))|do\(\)|don't\(\)")
         .unwrap()
         .find_iter(&input)
-        .map(|e| mul_parser::mul(e.as_str()).unwrap())
-        .sum();
-    println!("sum: {sum}");
+        .map(|e| match mul_parser::mul(e.as_str()).unwrap() {
+            Matched::Mul(m) if enabled => (m, m),
+            Matched::Mul(m) => (m, 0),
+            Matched::Do => {
+                enabled = true;
+                (0, 0)
+            }
+            Matched::DoNot => {
+                enabled = false;
+                (0, 0)
+            }
+        })
+        .fold((0, 0), |(a, b), (x, y)| (x + a, y + b));
+    println!("sum1: {sum1}, sum2: {sum2}");
 }
 
 peg::parser! {
@@ -21,7 +40,12 @@ peg::parser! {
         rule number() -> isize
             = n:$(['0'..='9']+) {? n.parse().or(Err("isize")) }
 
-        pub rule mul() -> isize
-            = "mul(" x:number() "," y:number() ")" { x * y }
+        pub rule mul() -> Matched =
+        precedence! {
+            "mul(" x:number() "," y:number() ")" { Matched::Mul(x * y) }
+            --
+            "do()" { Matched::Do}
+            "don't()" { Matched::DoNot}
+        }
     }
 }
